66f23916f55f7f2a9fdb62e44c68e284526441ef
Samuel Piwnicki <samcios102@gmail.com>
2019-09-12 21:25:57 +0200

    Controller and Extension Bind added

---
+++ src/Zoo.Api/Controllers/AnimalController.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Zoo.Application.Commands;
+using Zoo.Application.Commands.Animals;
+using Zoo.Application.Dto;
+using Zoo.Application.Queries;
+using Zoo.Application.Queries.Animals;
+
+namespace Zoo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalController : ControllerBase
+    {
+
+        private readonly ICommandDispatcher _commandDispatcher;
+        private readonly IQueryDispatcher _queryDispatcher;
+
+        public AnimalController(ICommandDispatcher commandDispatcher, IQueryDispatcher queryDispatcher)
+        {
+            _commandDispatcher = commandDispatcher;
+            _queryDispatcher = queryDispatcher;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AnimalDto>> GetByIdAsync([FromRoute] GetAnimal query)
+        {
+            var result = await _queryDispatcher.DispatchAsync<GetAnimal, AnimalDto>(query);
+
+            if (result is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AnimalDto>>> SearchAsync([FromQuery] SearchAnimals query)
+        {
+            var result = await _queryDispatcher.DispatchAsync<SearchAnimals, IEnumerable<AnimalDto>>(query);
+
+            if (result is null || !result.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAnimalAsync([FromBody] CreateAnimal command)
+        {
+            await _commandDispatcher.DispatchAsync(command);
+            return CreatedAtAction(nameof(GetByIdAsync), new {id = command.Id}, command);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> DeleteAnimalAsync([FromRoute] Guid id, [FromBody] UpdateAnimal command)
+        {
+            command.Bind(x => x.Id, id);
+
+            await _commandDispatcher.DispatchAsync(command);
+            return Ok();
+        }
+        
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAnimalAsync([FromRoute] Guid id)
+        {
+            await _commandDispatcher.DispatchAsync(new DeleteAnimal(id));
+            return Ok();
+        }
+
+
+    }
+}
--- src/Zoo.Api/Extensions.cs
+++ src/Zoo.Api/Extensions.cs
@@ -1,7 +1,37 @@
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Zoo.Application.Commands;
+
 namespace Zoo.Api
 {
-    public class Extensions
+    public static class Extensions
     {
-        
+        public static TCommand Bind<TCommand>(this TCommand model, Expression<Func<TCommand, Guid>> expression, Guid id) where TCommand : ICommand
+        {
+            var memberExpression = expression.Body as MemberExpression;
+            
+            
+            if (memberExpression is null)
+            {
+                memberExpression = ((UnaryExpression)expression.Body).Operand as MemberExpression;
+            }
+            
+            var propertyName = memberExpression.Member.Name.ToLowerInvariant();
+            
+            
+            var modelType = model.GetType();
+            var field = modelType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic)
+                .SingleOrDefault(x => x.Name.ToLowerInvariant().StartsWith($"<{propertyName}>"));
+            
+            if (field is null)
+            {
+                return model;
+            }
+            field.SetValue(model, id);
+
+            return model;
+        }
     }
 }
